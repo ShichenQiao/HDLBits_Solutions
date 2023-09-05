e7064a92f94d311b757450e439a494f533310cc2
Justin Qiao <justinqiao@Justins-MBP.attlocal.net>
2023-09-04 17:38:06 -0700

    mux

--- Circuits/Combinational_Logic/Basic_Gates/55_mt2015_q4.sv
+++ Circuits/Combinational_Logic/Basic_Gates/55_mt2015_q4.sv
@@ -1,5 +1,5 @@
 module top_module (input x, input y, output z);
-	wire z1, z2, z3, z4;
+    wire z1, z2, z3, z4;
     A IA1(x, y, z1);
     B IB1(x, y, z2);
     A IA2(x, y, z3);
--- Circuits/Combinational_Logic/Basic_Gates/58_popcount3.sv
+++ Circuits/Combinational_Logic/Basic_Gates/58_popcount3.sv
@@ -2,8 +2,8 @@ module top_module(
     input [2:0] in,
     output [1:0] out );
 
-	assign out = &in ? 2'b11:
-        		 ~|in ? 2'b00 :
-        		 ^in ? 2'b01:
-        		 2'b10;
+    assign out = &in ? 2'b11:
+        	 ~|in ? 2'b00 :
+        	 ^in ? 2'b01:
+        	 2'b10;
 endmodule
